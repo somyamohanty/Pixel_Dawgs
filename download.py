b694667c1b5a1ab5d456cb48660816928a830853
svk38 <svk38@msstate.edu>
2015-09-27 14:46:11 -0500

    Added txt file to keep track of downloads already performed

--- download.py
+++ download.py
@@ -7,13 +7,19 @@ def get_redirected_url(url):
 
 fi = open("C:/Users/nrosetti94/Desktop/yfcc100m_dataset-0", "r")
 
-done = 10000
+
+iter_file = open("download_tracker.txt", "r")
+done = iter_file.readline()
 for i in range(done):
     next(fi)
 
 for i in range(50000):
+    done+=1
     img_data = next(fi)
     id, url, isvideo = img_data.split("\t")[0], img_data.split("\t")[14], int(img_data.split("\t")[22].strip("\n")) == 1
     if (get_redirected_url(url) != "https://s.yimg.com/pw/images/en-us/photo_unavailable.png") and (isvideo == False):
         urllib.urlretrieve(url, "C:/Users/nrosetti94/Documents/Flickr/sampleimages/"+str(id)+".jpg")
 
+iter_file = open("download_tracker.txt", "w")
+iter_file.write(done)
+iter_file.close()
