d664407327da0d969893cb3e8bc108f4637b166b
Nick Rosetti <nrosetti94@gmail.com>
2015-09-28 11:25:37 -0500

    fixed additional crashes

--- download.py
+++ download.py
@@ -1,8 +1,11 @@
 import urllib, urllib2
 
 def get_redirected_url(url):
-    opener = urllib2.build_opener(urllib2.HTTPRedirectHandler)
-    request = opener.open(url)
+    try:
+        opener = urllib2.build_opener(urllib2.HTTPRedirectHandler)
+        request = opener.open(url)
+    except:
+        return ''
     return request.url
 
 fi = open("trainingset/yfcc100m_dataset-0", "r")
@@ -10,20 +13,26 @@ fi = open("trainingset/yfcc100m_dataset-0", "r")
 
 iter_file = open("download_tracker.txt", "r")
 
-done = int(iter_file.readline())
+try:
+    done = int(iter_file.readline())
+except ValueError:
+    done = 0
+
+iter_file = open("download_tracker.txt", "w")
 
 for i in range(done):
     next(fi)
 
-iter_file = open("download_tracker.txt", "w")
-
 for i in range(100000):
-    done+=1
+    done += 1
     img_data = next(fi)
     id, url, isvideo = img_data.split("\t")[0], img_data.split("\t")[14], int(img_data.split("\t")[22].strip("\n")) == 1
-    if (get_redirected_url(url) != "https://s.yimg.com/pw/images/en-us/photo_unavailable.png") and (isvideo == False):
+    try:
+        redirected = get_redirected_url(url)
+        if (redirected != "https://s.yimg.com/pw/images/en-us/photo_unavailable.png") and redirected != '' and (isvideo is False):
+            urllib.urlretrieve(url, "sampleimages/"+str(id)+".jpg")
+    finally:
         iter_file.seek(0)
         iter_file.write(str(done))
-        urllib.urlretrieve(url, "sampleimages/"+str(id)+".jpg")
-
+        
 iter_file.close()
