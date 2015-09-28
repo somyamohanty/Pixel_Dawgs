7f58214d26b6c9a1313f4860c9b745ee5a63cf4d
Shreya Gupta <shreyagupta995@gmail.com>
2015-09-28 09:15:01 -0500

    Changes to error handling

--- download_shreya.py
+++ download_shreya.py
@@ -5,7 +5,7 @@ def get_redirected_url(url):
         opener = urllib2.build_opener(urllib2.HTTPRedirectHandler)
         request = opener.open(url)
     except:
-        raise Exception
+        return "https://s.yimg.com/pw/images/en-us/photo_unavailable.png"
     return request.url
 
 fi = open("trainingset/yfcc100m_dataset-2", "r")
@@ -26,11 +26,13 @@ for i in range(done):
 for i in range(100000):
     done += 1
     img_data = next(fi)
-    id, url, isvideo = img_data.split("\t")[0], img_data.split("\t")[14], int(img_data.split("\t")[22].strip("\n")) == 1
+    data = img_data.split("\t")
+    id, url, isvideo = data[0], data[14], int(data[22].strip("\n")) == 1
     try:
         if (get_redirected_url(url) != "https://s.yimg.com/pw/images/en-us/photo_unavailable.png") and (isvideo is False):
             urllib.urlretrieve(url, "sampleimages/"+str(id)+".jpg")
     finally:
         iter_file.seek(0)
         iter_file.write(str(done))
+
 iter_file.close()
