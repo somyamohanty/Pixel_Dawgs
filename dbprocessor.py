adf552b6835b896fbb32501eece39199a66a1851
svk38 <svk38@msstate.edu>
2015-11-23 23:02:00 -0600

    added files required to process tagged images data

---
+++ dbprocessor.py
@@ -0,0 +1,31 @@
+__author__ = 'SATYANARAYANAREDDY'
+
+import csv
+files = ["satya.csv", "manish.csv", "shreya.csv", "nick.csv"]
+
+count = 0
+image_data = {}
+list_of_images_tagged = []
+for file in files:
+    csvfile = open(file, 'r')
+    csv_dict = csv.DictReader(csvfile)
+    for row in csv_dict:
+        if row['tag6']!= "NULL":
+            image_data[row['imgid']] = row
+            list_of_images_tagged.append(row['imgid'])
+            count += 1
+print count,"images have been tagged in all"
+
+#some string processing to make a list of coordinates separated by a colon
+li = ['tag1c', 'tag2c', 'tag3c', 'tag4c', 'tag5c', 'tag6c', 'tag7c', 'tag8c', 'tag9c', 'tag10c']
+for id in list_of_images_tagged:
+    for each in li:
+        if image_data[id][each] != '':
+            image_data[id][each] = image_data[id][each][:-1].split(",")
+
+#example usage - retrieving the tag6, and tag6c for all images
+for id in list_of_images_tagged:
+    print image_data[id]['tag6'], image_data[id]['tag6c']
+
+#example instance of image data
+print(image_data["1405589924"])
